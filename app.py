2ac6715a09dbabd329c309eec5c404858ee76e08
NaHyeong <donadio912@naver.com>
2023-05-18 15:09:30 +0900

    수정본

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-
+from datetime import datetime
 from pymongo import MongoClient
 from bson import ObjectId
 import certifi
